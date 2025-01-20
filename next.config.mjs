a78a512dbdb1f31e5210a5a5c51dc9f3500829cd
Augusto-develop <augustogomes0822@gmail.com>
2025-01-20 12:14:21 -0300

    dev

--- next.config.mjs
+++ next.config.mjs
@@ -45,9 +45,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    runtime: 'nodejs',  // Adiciona a configuração experimental
-  },
   images: {
     remotePatterns: [
       {
